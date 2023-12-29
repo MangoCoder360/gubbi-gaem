6e2ea244e3131010426cb0652b1c434b1acedbe9
minerpix <105188990+minerpix@users.noreply.github.com>
2023-12-28 23:16:21 -0800

    fixed letters animation

--- main.py
+++ main.py
@@ -87,4 +87,4 @@ def submitguess(gameID, playerNumber, guess):
     return redirect("/game/"+str(gameID))
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=8000)
